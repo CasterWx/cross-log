0e5e552c1b28b48608026f0338b7f83e203c90aa
AntzUhl <antzuhl1998@gmail.com>
2020-11-26 11:00:13 +0800

    feature(conf/code): 初始化仓库

---
+++ README.md
@@ -0,0 +1,8 @@
+# Cross Log
+
+分布式调用链日志追踪分析系统.
+
+调用链依赖: cat
+日志存储分析: elasticsearch
+日志同步: kafka
+more...
---
+++ src/main/java/com/antzuhl/cross/CrossLogApplication.java
@@ -0,0 +1,17 @@
+package com.antzuhl.cross;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author AntzUhl
+ * @Date 2020/11/26 10:52
+ * @Description 启动类
+ */
+@SpringBootApplication
+public class CrossLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrossLogApplication.class, args);
+    }
+}
